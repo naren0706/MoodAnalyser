d376218de0d06d4e98c35df1ea626542f519d32f
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-04 10:09:43 +0530

    [Narenthra Kishore] Add : Create mood analyser object and test it

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        }
+    }
+}
--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -1,16 +1,23 @@
+using System.Security.Cryptography.X509Certificates;
+using MoodAnalyserProblem;
 namespace MoodAnalyserTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+        MoodAnalyser analyser = new MoodAnalyser();
         [Test]
-        public void Test1()
+        public void GivenSadMessage_WhenAnalye_ShouldReturnSad()
         {
-            Assert.Pass();
+            string message = "I am in sad mood";
+            string actual = analyser.AnalyseMood(message);
+            Assert.AreEqual(actual, "SAD");
+        }
+        [Test]
+        public void GivenSadMessage_WhenAnalye_ShouldReturnHappy()
+        {
+            string message = "I am in any mood";
+            string actual = analyser.AnalyseMood(message);
+            Assert.AreEqual(actual, "HAPPY");
         }
     }
 }
